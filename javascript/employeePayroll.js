a5764ef13399a7bfc411c1d0bb379b7bc6f7f041
vishalchandra736 <vishalchandra736@gmail.com>
2023-02-13 01:22:12 +0530

    Added : Validate name  and date on Employee payroll app home page

--- javascript/employeePayroll.js
+++ javascript/employeePayroll.js
@@ -102,3 +102,52 @@ window.addEventListener('DOMContentLoader', (event) => {
         output.textContent = salary.value;
     });
 });
+
+const save = () => {
+    try {
+        let EmployeePayrollData = createEmployeePayroll();
+    }
+    catch(e) {
+        return;
+    }
+}
+
+const createEmployeePayroll = () => {
+    let employeePayrollData = new EmployeePayrollData();
+    try {
+        employeePayrollData.name = getInputValueById('#name');
+    }
+    catch(e) {
+        setTextValue(".text-error", e);
+        throw e;
+    }
+    employeePayrollData.profilePic = getSelectedValues('[name=profile]').pop();
+    employeePayrollData.gender = getSelectedValues('[name=gender]').pop();
+    employeePayrollData.department = getSelectedValues('[name=department]');
+    employeePayrollData.salary = getInputValueById('#salary');
+    employeePayrollData.notes = getInputValueById('#notes');
+    let date = getInputValueById('#day') + " " + getInputValueById('#month') + " " + getInputValueById('#year');
+    employeePayrollData.date = Date.parse(date);
+    alert(employeePayrollData.toString());
+    return employeePayrollData;
+} 
+
+const getSelectedValues = (propertyValue) => {
+    let allItems = document.querySelectorAll(propertyValue);
+    let selItems = [];
+    allItems.forEach(item => {
+        if(item.checked)
+            selItems.push(item.value);
+    });
+    return selItems;
+}
+
+const getInputValueById = (id) => {
+    let value = document.querySelector(id).value;
+    return value;
+}
+
+const getInputElementValue = (id) => {
+    let value = document.getElementById(id).value;
+    return value;
+}
