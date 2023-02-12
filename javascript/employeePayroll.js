a0480fbbd5b6a41e1eb47a09e65d97ce251c92ca
vishalchandra736 <vishalchandra736@gmail.com>
2023-02-13 00:24:11 +0530

    Added : Set listeners in salary and name using javascript on Employee payroll app home page

--- javascript/employeePayroll.js
+++ javascript/employeePayroll.js
@@ -77,3 +77,28 @@ class EmployeePayrollData {
                         + ", department = " + this.department + ", salary = " + this.salary + ", startDate = " + this.startDate + ", note = " + this.note;
     }
 }
+
+window.addEventListener('DOMContentLoader', (event) => {
+    const name = document.querySelector('#name');
+    const textError = document.querySelector('.text-error');
+    name.addEventListener('input', function() {
+        if(name.value.length == 0) {
+            textError.textContent = "";
+            return;
+        }
+        try {
+            (new EmployeePayrollData()).name = name.value;
+            textError.textContent = "";
+        }      
+        catch (e) {
+            textError.textContent = e;
+        }
+    });
+
+    const salary = document.querySelector('#salary');
+    const output = document.querySelector('.salary-output');
+    output.textContent = salary.value;
+    salary.addEventListener('input', function() {
+        output.textContent = salary.value;
+    });
+});
