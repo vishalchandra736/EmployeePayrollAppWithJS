0a9e6358b8e8661f3fdb347fca04d171431199bf
vishalchandra736 <vishalchandra736@gmail.com>
2023-02-13 14:02:55 +0530

    Added : Reset the Employee payroll app home page

--- javascript/employeePayroll.js
+++ javascript/employeePayroll.js
@@ -164,3 +164,32 @@ function createAndUpdateStorage(employeePayrollData) {
     alter(employeePayrollList.toString());
     localStorage.setItem("EmployeePayrollList", JSON.stringify(employeePayrollList));
 }
+
+const resetForm = () => {
+    setValue('#name', '');
+    unsetSelectedValues('[name=profile]');
+    unsetSelectedValues('[name=gender]');
+    unsetSelectedValues('[name=department]');
+    setValue('#salary', '');
+    setValue('#notes', '');
+    setValue('#day', '1');
+    setValue('#month', 'January');
+    setValue('#year', '2020');
+}
+
+const unsetSelectedValues = (propertyValue) => {
+    let allItems = document.querySelectorAll(propertyValue);
+    allItems.forEach(item => {
+        item.checked = false;
+    });
+}
+
+const setTextValue = (id, value) => {
+    let allItems = document.querySelector(id);
+    Element.textContent = value;
+}
+
+const setValue = (id, value) => {
+    const element = document.querySelector(id);
+    element.value = value;
+}
